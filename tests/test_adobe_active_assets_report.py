9d1fc4197164943c134bfcdc6f43d1d8df233524
Carlos Anuarbe <Carlos.Anuarbe@ingrammicro.com>
2022-12-22 17:25:35 +0100

    Code format and test fixes

--- tests/test_adobe_active_assets_report.py
+++ tests/test_adobe_active_assets_report.py
@@ -6,6 +6,7 @@
 
 from reports import utils
 from reports.assets import entrypoint
+from reports import api_calls
 
 # input_data as passed to generate
 input_data = {
@@ -21,7 +22,8 @@ input_data = {
 }
 
 
-def test_assets_report(assets, listing, extra_context_callback, sync_client_factory, response_factory, pricelist_version, pricelist_points):
+def test_assets_report(assets, listing, extra_context_callback, sync_client_factory, response_factory,
+                       pricelist_version, pricelist_points):
     responses = [
         response_factory(
             query='and(eq(marketplace.id,MP-65669),eq(product.id,PRD-207-752-513),eq(status,listed))',
@@ -36,9 +38,19 @@ def test_assets_report(assets, listing, extra_context_callback, sync_client_fact
             value=[pricelist_points],
         ),
     ]
+    client_assets = sync_client_factory([
+        response_factory(
+            query='and(ge(events.created.at,2022-12-1T00:00:00),le(events.created.at,2022-12-15T00:00:00),'
+                  'in(product.id,(PRD-207-752-513)),eq(status,active))',
+            value=[assets],
+        ),
+    ])
+
+    assets = api_calls.request_assets(client_assets, input_data)[0]
+
     list_to_assert = []
     for asset in assets:
-        # need to be rebuilded for each asset
+        # client need to be rebuilded for each asset
         client = sync_client_factory(responses)
         marketplace_params = utils.get_marketplace_params(client, asset, testing=True)
         if not marketplace_params:
@@ -46,3 +58,30 @@ def test_assets_report(assets, listing, extra_context_callback, sync_client_fact
         list_to_assert.append(utils.process_line(asset, entrypoint.asset_headers,
                                                  entrypoint.asset_params_headers, marketplace_params))
     assert len(list_to_assert) == len(assets)
+
+
+def test_api_calls(sync_client_factory, response_factory, assets, listing, pricelist_version):
+    responses = [
+        response_factory(
+            query='and(ge(events.created.at,2022-12-1T00:00:00),le(events.created.at,2022-12-15T00:00:00),'
+                  'in(product.id,(PRD-207-752-513)),eq(status,active))',
+            value=True,
+        ),
+        response_factory(
+            query='and(eq(marketplace.id,MP-65669),eq(product.id,PRD-207-752-513),eq(status,listed))',
+            value=True,
+        ),
+        response_factory(
+            query='and(eq(pricelist.id,PL-667-945-709),eq(status,active))',
+            value=True,
+        ),
+        response_factory(
+            query='eq(status,filled)',
+            value=True,
+        ),
+    ]
+    client = sync_client_factory(responses)
+    assert api_calls.request_assets(client, input_data)
+    assert api_calls.request_listing(client, assets[0]['marketplace']['id'], assets[0]['product']['id'])
+    assert api_calls.request_price_list(client, listing['pricelist']['id'])
+    assert api_calls.request_price_list_version_points(client, pricelist_version['id'], testing=True)
